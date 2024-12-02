bc80ecb0157fe6190f5fbff620713f5e2e757ee1
Mélina Hillion <114908960+melinahillion@users.noreply.github.com>
2024-12-02 17:30:12 +0100

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -4,8 +4,8 @@ set -x
 echo "Début du script"
 
 # Cloner le dépôt GitHub
-echo "Clonage du dépôt..."
-git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git || { echo "Échec du clonage"; exit 1; }
+#echo "Clonage du dépôt..."
+#git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git || { echo "Échec du clonage"; exit 1; }
 
 # Naviguer vers le répertoire du notebook
 echo "Accès au répertoire du notebook..."
@@ -13,6 +13,7 @@ cd methodes_ensemblistes_notebooks/Python || { echo "Échec de l'accès au répe
 
 # Lancer Jupyter Notebook
 echo "Lancement de Jupyter Notebook..."
-jupyter-notebook --NotebookApp.default_url=/work/methodes_ensemblistes_notebooks/Python/classification_binaire_rf.ipynb || { echo "Échec du lancement de Jupyter"; exit 1; }
+jupyter notebook classification_binaire_rf.ipynb || { echo "Échec du lancement de Jupyter"; exit 1; }
 
 echo "Fin du script"
+
