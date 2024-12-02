b3ac6afa952a13f2a149d09897d1c75aa43ddc43
Mélina Hillion <114908960+melinahillion@users.noreply.github.com>
2024-12-02 15:23:52 +0100

    Create init_script.sh

---
+++ init_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Cloner le dépôt GitHub dans le répertoire de travail
+git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git
+
+# Naviguer vers le répertoire contenant le notebook qui nous intéresse
+cd methodes_ensemblistes_notebooks/Python
+
+# Lancer le service en ouvrant le notebook qui nous intéresse
+jupyter notebook classification_binaire_rf.ipynb
