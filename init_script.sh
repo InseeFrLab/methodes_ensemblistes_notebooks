b552c4b2f5af31bc4d18e3c32924cc80a8be8160
mhillion <melina.hillion@insee.fr>
2024-12-03 09:26:51 +0000

    nouveau script d'init

--- init_script.sh
+++ init_script.sh
@@ -2,29 +2,14 @@
 echo "Début du script"
 
 # Cloner le dépôt GitHub
-#echo "Clonage du dépôt..."
-# rm -rf dt
-# git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git dt || { echo "Échec du clonage"; exit 1; }
-
-# Methode 2
 WORK_DIR="/home/onyxia/work"
 DOWNLOAD_URL="https://raw.githubusercontent.com/InseeFrLab/methodes_ensemblistes_notebooks/refs/heads/main/Python/classification_binaire_rf.ipynb"
 echo $DOWNLOAD_URL
 curl -L $DOWNLOAD_URL -o "${WORK_DIR}/classification_binaire_rf.ipynb"
 
-
-# Naviguer vers le répertoire du notebook
-#echo "Accès au répertoire du notebook..."
-#cd dt/Python || { echo "Échec de l'accès au répertoire"; exit 1; }
-
 # Lancer Jupyter Notebook
 echo "Ouverture du notebook..."
-# quarto render classification_binaire_rf.ipynb || { echo "Échec du lancement de Jupyter"; exit 1; }
 echo "c.LabApp.default_url = '/lab/tree/classification_binaire_rf.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
-
-
-
-# Download the notebook directly using curl
 
 
 echo "Fin du script"
