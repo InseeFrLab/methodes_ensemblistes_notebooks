5f7ead087dc7f2470bbd518e73fa6c7685f9eeb8
mhillion <melina.hillion@insee.fr>
2024-12-03 09:13:03 +0000

    Script d'init par copier coller

--- init_script.sh
+++ init_script.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-set -x
-
 echo "Début du script"
 
 # Cloner le dépôt GitHub
 #echo "Clonage du dépôt..."
-#git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git || { echo "Échec du clonage"; exit 1; }
+# rm -rf dt
+# git clone https://github.com/InseeFrLab/methodes_ensemblistes_notebooks.git dt || { echo "Échec du clonage"; exit 1; }
+
+# Methode 2
+WORK_DIR="/home/onyxia/work"
+DOWNLOAD_URL="https://raw.githubusercontent.com/InseeFrLab/methodes_ensemblistes_notebooks/refs/heads/main/Python/classification_binaire_rf.ipynb"
+echo $DOWNLOAD_URL
+curl -L $DOWNLOAD_URL -o "${WORK_DIR}/classification_binaire_rf.ipynb"
+
 
 # Naviguer vers le répertoire du notebook
-echo "Accès au répertoire du notebook..."
-cd Python || { echo "Échec de l'accès au répertoire"; exit 1; }
+#echo "Accès au répertoire du notebook..."
+#cd dt/Python || { echo "Échec de l'accès au répertoire"; exit 1; }
 
 # Lancer Jupyter Notebook
-echo "Lancement de Jupyter Notebook..."
-jupyter notebook classification_binaire_rf.ipynb || { echo "Échec du lancement de Jupyter"; exit 1; }
+echo "Ouverture du notebook..."
+# quarto render classification_binaire_rf.ipynb || { echo "Échec du lancement de Jupyter"; exit 1; }
+echo "c.LabApp.default_url = '/lab/tree/classification_binaire_rf.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+
+
+
+# Download the notebook directly using curl
+
 
 echo "Fin du script"
